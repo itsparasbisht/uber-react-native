c74686fbee9afb16f97f87ca93406077991a6a3e
itsparasbisht <iamparasbisht@gmail.com>
2023-04-23 20:32:38 +0530

    NavOptions component

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,22 @@
-import { Text, View } from "react-native";
 import React from "react";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "twrnc";
+import NavOptions from "../components/NavOptions";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Home Screen</Text>
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{ uri: "https://links.papareact.com/gzs" }}
+        />
+        <NavOptions />
       </View>
     </SafeAreaView>
   );

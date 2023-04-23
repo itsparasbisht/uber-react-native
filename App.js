d74bf3b0bce0404d7dea69f19561be045e9e871b
itsparasbisht <iamparasbisht@gmail.com>
2023-04-23 20:02:17 +0530

    npm safe area view

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
---
+++ screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Home Screen</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;

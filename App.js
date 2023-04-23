8d2173b6d470c224635b7a99516a37b1e482b80b
itsparasbisht <iamparasbisht@gmail.com>
2023-04-23 20:41:07 +0530

    changes

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <HomeScreen />
-    </Provider>
+    <SafeAreaProvider>
+      <Provider store={store}>
+        <HomeScreen />
+      </Provider>
+    </SafeAreaProvider>
   );
 }
 

71de87acca9a798737b1c8a15e7df1186c6a1fe6
itsparasbisht <iamparasbisht@gmail.com>
2023-04-26 20:30:33 +0530

    navoptions styles

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,6 +1,7 @@
 import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import tw from "twrnc";
+import { Icon } from "@rneui/themed";
 
 const data = [
   {
@@ -24,7 +25,9 @@ const NavOptions = () => {
       keyExtractor={(item) => item.id}
       horizontal
       renderItem={({ item }) => (
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40 rounded-4`}
+        >
           <View>
             <Image
               style={{
@@ -34,7 +37,13 @@ const NavOptions = () => {
               }}
               source={{ uri: item.image }}
             />
-            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Text style={tw`mt-2 text-lg font-extrabold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name="arrowright"
+              type="antdesign"
+              color="white"
+            />
           </View>
         </TouchableOpacity>
       )}

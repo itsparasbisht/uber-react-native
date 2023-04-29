c9f773a5a627cd84dd20da11eb3921e34ca02075
itsparasbisht <iamparasbisht@gmail.com>
2023-04-29 22:50:52 +0530

    dispatch origin details

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,8 +4,12 @@ import tw from "twrnc";
 import NavOptions from "../components/NavOptions";
 import { GOOGLE_MAPS_APIKEY } from "@env";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <View style={tw`p-5`}>
       <Image
@@ -27,13 +31,21 @@ const HomeScreen = () => {
             fontSize: 18,
           },
         }}
-        debounce={1000}
+        debounce={500}
         query={{ key: GOOGLE_MAPS_APIKEY, language: "en" }}
         minLength={2}
         enablePoweredByContainer={false}
         fetchDetails={true}
-        onPress={(data, details = null) => {
-          console.log(data, details);
+        onPress={(data, details) => {
+          console.log(data);
+          dispatch(
+            setOrigin({
+              location: details.geometry.location,
+              description: data.description,
+            })
+          );
+
+          dispatch(setDestination(null));
         }}
       />
       <NavOptions />
